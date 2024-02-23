ab5658e429f96ed32f9ec9656a1ac90297ab3c54
RafaelAugustoMoraes <159540509+RafaelAugustoMoraes@users.noreply.github.com>
2024-02-23 08:49:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var agom=11;
+
+function amongu() {
+    var ip=54;
+    return ip;
+}
+
+if(10>0) {
+    console.log('verdadeiro');
+
+}else{
+    console.log('falso');
+}
+
+console.log(amongu());
+console.log(agom);
